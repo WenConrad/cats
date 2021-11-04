5a33e5e4309e8c1697478774f7a05f6a7adc08e5
WenConrad <conradwen@gmail.com>
2021-11-04 10:59:30 -0700

    async function return example

---
+++ asyncBreeds.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, functionDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) functionDone(data);
+  });
+};
+
+const printCatBreed = (breed) => {
+  console.log('Return Value: ', breed);
+}
+breedDetailsFromFile('Bombay', printCatBreed);
